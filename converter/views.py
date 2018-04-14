961f112e52d2271e78bb4d22f9b4aa63baee6b76
Calvin Wu <calvin620707@msn.com>
2018-04-14 22:05:51 +0800

    csv converter

--- converter/views.py
+++ converter/views.py
@@ -1,25 +1,34 @@
+import csv
 import logging
+from io import TextIOWrapper
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-
 # Create your views here.
 from converter.forms import UploadFileForm
-
 
 logger = logging.getLogger(__name__)
 
 
 def upload_file(request):
-    logger.info("Test")
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            logger.info(request.FILES['file'].read())
-            # handle_uploaded_file(request.FILES['file'])
+            __convert_csv(request.FILES['file'])
             return HttpResponseRedirect('/')
     else:
         form = UploadFileForm()
     return render(request, 'converter/index.html', {'form': form})
 
+
+def __convert_csv(in_f):
+    f = TextIOWrapper(in_f, encoding='big5')
+
+    # skip first 2 lines for account info and headers
+    for _ in range(2):
+        f.readline()
+
+    reader = csv.reader(f)
+    for row in reader:
+        logging.info(row)
