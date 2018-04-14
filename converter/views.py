8e46f204ef9460b9f10d9d54d9c7691523e0abfa
Calvin Wu <calvin620707@msn.com>
2018-04-14 23:05:38 +0800

    cat done

--- converter/views.py
+++ converter/views.py
@@ -1,6 +1,8 @@
 import csv
 import logging
+from collections import defaultdict
 from io import TextIOWrapper
+from datetime import date, datetime
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -30,5 +32,29 @@ def __convert_csv(in_f):
         f.readline()
 
     reader = csv.reader(f)
+    ret = defaultdict(dict)
     for row in reader:
-        logging.info(row)
+        category = None
+        if 'Y177748' in row[5]:
+            category = '中華電信'
+
+        if '北市水費' in row[4]:
+            category = '水費'
+
+        if '台電電費' in row[4]:
+            category = '電費'
+
+        if '信用卡款' in row[4] and '國泰世華卡' in row[5]:
+            category = 'koko卡費'
+
+        if '604056495' in row[5]:
+            category = '瓦斯'
+
+        if not category:
+            logging.info("{} no category".format(row))
+            continue
+
+        row_date = datetime.strptime(row[0], '%Y%m%d').date()
+        spent = int(row[1])
+        ret[row_date.strftime("%Y%m")].update({category: spent})
+    print(ret)
